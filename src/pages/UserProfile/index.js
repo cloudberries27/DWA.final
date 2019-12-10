0d3452a6da5e4a5bce7e27fdfbb57ff7da1def67
cloudberries27 <ccrod2298@gmail.com>
2019-12-10 15:15:22 -0500

    user fixed

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -3,7 +3,8 @@ import * as firebase from "firebase/app";
 import UserInformation from "../../components/UserProfileComponents/index";
 
 export default function UserProfile({ user }){
-	user.updateProfile({
+	var currUser = firebase.auth().currentUser;
+	currUser.updateProfile({
 		displayName: "Rebecca Foster",
 		photoURL: "https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80"
 	  }).then(function() {
@@ -13,7 +14,7 @@ export default function UserProfile({ user }){
 	  });
 	return (
 		<div className='user-profile'>
-			<img alt='profile picture' src={user.photoURL}/>
+			
 			<h1>{user.displayName}</h1>
 			<UserInformation email={user.email ? user.email : 'oh no'}/>
 		</div>
